5e24bb6e92d2de663a75e174b248ff100c72b815
mehran <mehranmohamadi1311@gmail.com>
2026-05-04 14:40:09 +0330

    docs(tailwind): set up tailwindcss for styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <>Books explorer</>;
+  return (
+    <div className="text-center text-white bg-green-400 text-4xl p-12 m-2">
+      Books explorer
+    </div>
+  );
 }
 
 export default App;
